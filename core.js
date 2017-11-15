6a6831e6e97d3988bbc519425bd2eb4c4af96ff7
pankeyu <pankeyu@baidu.com>
2017-11-15 11:43:06 +0800

    change to english & arrayUnique

--- core.js
+++ core.js
@@ -1,27 +1,27 @@
 /**
- * @file ytool核心
+ * @file ytool core
  * @author coypan
  */
 
 var ytool = {
 
     /**
-     * 判断变量的类型
+     * check the type of the parameter
      *
-     * @param {any} arg 需要进行类型判断的变量.
-     * @return {string} 变量的类型。String,Funciton,Object,Null,Undefined,Boolean,Number,RegExp等等。
+     * @param {any} param the parameter need to be checked
+     * @return {string} this type of the parameter. such as String,Funciton,Object,Null,Undefined,Boolean,Number,RegExp and so on。
      *
      */
-    checkType: function (arg) {
+    checkType: function (param) {
         var reg = /\[object\s(\w+)\]/;
-        return Object.prototype.toString.call(arg).replace(reg, '$1');
+        return Object.prototype.toString.call(param).replace(reg, '$1');
     },
 
     /**
-     * 获取url中的某参数的值
+     * get query from the url
      *
-     * @param {string} param 想要获取的参数的值
-     * @return {string|undefined} 参数的值。若该参数不存在，返回undefined;
+     * @param {string} param the query
+     * @return {string|undefined} the value of the key get from the url
      */
     getUrlQuery: function (param) {
         var url = window.location.search.substr(1);
@@ -31,6 +31,22 @@ var ytool = {
             return res[2];
         }
         return undefined;
+    },
+
+    /**
+     * Array unique
+     *
+     * @param {Array} arr the array need to be unique
+     * @return {Array} new array
+     *
+     */
+    arrayUnique: function (arr) {
+        if (this.checkType(arr) !== 'Array') {
+            return arr;
+        }
+        return arr.filter(function (item, idx, array) {
+            return array.indexOf(item) === idx;
+        });
     }
 
 };
